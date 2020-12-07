35e385588aebebad759a3c0ed31000c3d7ff50d0
Upit Alex <upitsoft@gmail.com>
2020-12-07 17:26:43 +0500

    Ping handle.

---
+++ Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Watchdog
+{
+    internal static class Logger
+    {
+        private static StreamWriter streamWriter;
+        
+        public static void InitStreamFile()
+        {
+            string path = $"{AppDomain.CurrentDomain.BaseDirectory}/log{GetStringFromDate(DateTime.Now, true)}.txt";
+            streamWriter = new StreamWriter(path, true) {AutoFlush = true};
+        }
+
+        public static void CloseStreamFile()
+        {
+            streamWriter.Close();
+        }
+        
+        public static void Log(string data)
+        {
+            string logLine = $"[{GetStringFromDate(DateTime.Now)}]: {data}";
+            Console.WriteLine(logLine);
+            streamWriter.WriteLine(logLine);
+
+            // Console.Write("Press  to exit... ");
+            // while (Console.ReadKey().Key != ConsoleKey.Enter)
+            // {
+            //     //run loop until Enter is press
+            // }
+        }
+
+        private static string GetStringFromDate(DateTime time, bool simple = false)
+        {
+            return simple
+                ? $"{time.Day:00}{time.Month:00}{time.Year}_{time.Hour:00}{time.Minute:00}{time.Second:00}"
+                : $"{time.Day:00}.{time.Month:00}.{time.Year}-{time.Hour:00}:{time.Minute:00}:{time.Second:00}";
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,30 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Net.NetworkInformation;
+using System.Text;
 using System.Threading;
 
 namespace Watchdog
 {
     internal static class Program
     {
-        private const string ARG_PERIOD = "period";    // Период отправки запросов.
+        private const string ARG_DELAY = "delay";      // Задержка/Период отправки запросов.
+        private const string ARG_ATTEMPTS = "attempts";   // Кол-во попыток отправки подряд.
         private const string ARG_REJECTS = "rejects";  // Кол-во не полученных ответов для триггера.
         private const string ARG_HOST = "host";        // Хост для пинга.
+        private const string ARG_TIMEOUT = "timeout";
         private const string ARG_SCRIPT = "script";    // Выполнить после тригера.
 
-        private static StreamWriter streamWriter;
+        private static Dictionary<string, string> argsDic;
+        
         public static void Main(string[] args)
         {
-             InitStreamFile("log.txt");
+            Logger.InitStreamFile();
+            argsDic = GetArgs(args,
+                new[] {ARG_DELAY, ARG_ATTEMPTS, ARG_REJECTS, ARG_HOST, ARG_TIMEOUT, ARG_SCRIPT});
             
-            var argsDic = GetArgs(args, new[] {ARG_PERIOD, ARG_REJECTS, ARG_HOST, ARG_SCRIPT});
-            Thread.Sleep(5000);
+            PingLoop();
+            
             Exit();
+        }
+
+        private static void PingLoop()
+        {
+            if (!int.TryParse(argsDic[ARG_DELAY], out int delay))
+            {
+                Logger.Log(ARG_DELAY + " parse error.");
+                Exit();
+                return;
+            }
+
+            delay *= 60;
+            string host = argsDic[ARG_HOST];
+            int attempts = int.Parse(argsDic[ARG_ATTEMPTS]);
+            int timeout = int.Parse(argsDic[ARG_TIMEOUT]);
+            int rejects = int.Parse(argsDic[ARG_REJECTS]);
+            string script = argsDic[ARG_SCRIPT];
+
+            int reject = 0;
+            do
+            {
+                if (!Ping(host, attempts, timeout))
+                {
+                    reject++;
+                    Logger.Log($"Ping rejected. Total rejects {reject} of {rejects}");
+                    if (reject > rejects)
+                    {
+                        Logger.Log($"Ping max rejects. Executing {script}...");
+                        reject = 0;
+                        Execute(script);
+                    }
+                }
+                Logger.Log($"Waiting for next attempt {delay}s");
+                Thread.Sleep(delay * 1000);
+            } while (true);
+            
+        }
+
+        private static bool Ping(string host, int attempts, int timeout)
+        {
+            Ping pingSender = new Ping ();
+            PingOptions options = new PingOptions {DontFragment = true};
+
+            var emptyBuffer = Encoding.ASCII.GetBytes("");
+            for (int i = 0; i < attempts; i++)
+            {
+                Logger.Log($"Ping host {host}. Timeout: {timeout}ms. Attempt: {i}");
+                PingReply reply = pingSender.Send (host, timeout, emptyBuffer, options);
+                if (reply != null && reply.Status == IPStatus.Success)
+                {
+                    Logger.Log($"Reply from {host} recieved.");
+                    return true;
+                }
+            }
+            
+            return false;
+            // Console.WriteLine ("Address: {0}", reply.Address.ToString ());
+            // Console.WriteLine ("RoundTrip time: {0}", reply.RoundtripTime);
+            // Console.WriteLine ("Time to live: {0}", reply.Options.Ttl);
+            // Console.WriteLine ("Don't fragment: {0}", reply.Options.DontFragment);
+            // Console.WriteLine ("Buffer size: {0}", reply.Buffer.Length);
         }
 
         private static Dictionary<string, string> GetArgs(string[] args, string[] definedArgs)
         {
             var result = new Dictionary<string, string>(definedArgs.Length);
+            int missedArgs = 0;
             for (int i = 0; i < definedArgs.Length; i++)
             {
                 string definedArg = definedArgs[i];
@@ -41,38 +109,34 @@ namespace Watchdog
                         break;
                     }
                 }
+
                 if (!hasArg)
-                    Log("Argument was not found or has wrong value: " + definedArg);
+                {
+                    Logger.Log("Argument was not found or has wrong value: " + definedArg);
+                    missedArgs++;
+                }
+                    
             }
+
+            if (missedArgs > 0)
+            {
+                Logger.Log($"Missed {missedArgs} arguments. Exiting...");
+                Exit();
+            }
+                
 
             return result;
         }
 
-        private static void InitStreamFile(string path)
-        {
-            path = $"{AppDomain.CurrentDomain.BaseDirectory}/{path}";
-            streamWriter = new StreamWriter(path, true) {AutoFlush = true};
-        }
-
-        private static void Log(string data)
-        {
-            DateTime now = DateTime.Now;
-            var logLine =
-                $"[{now.Day:00}.{now.Month:00}.{now.Year}-{now.Hour:00}:{now.Minute:00}:{now.Second:00}]: {data}";
-            Console.WriteLine(logLine);
-            
-            streamWriter.WriteLine(logLine);
-
-            // Console.Write("Press  to exit... ");
-            // while (Console.ReadKey().Key != ConsoleKey.Enter)
-            // {
-            //     //run loop until Enter is press
-            // }
-        }
-
         private static void Exit()
         {
-            streamWriter.Close();
+            Logger.CloseStreamFile();
+            Environment.Exit(0);
+        }
+
+        private static void Execute(string path)
+        {
+            
         }
         
     }
